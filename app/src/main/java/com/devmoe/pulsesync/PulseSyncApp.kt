5bf06b0f195518674acabb0485f03179e9b4475f
Dev-moe-kyawaung <moekyawaung2027@gmail.com>
2026-04-18 18:09:45 +0630

    Create PulseSyncApp.kt

---
+++ app/src/main/java/com/devmoe/pulsesync/PulseSyncApp.kt
@@ -0,0 +1,9 @@
+package com.devmoe.pulsesync
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PulseSyncApp : Application() {
+    // Ready for Microsoft Azure App Center or Azure Functions integration
+}
